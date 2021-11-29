3378d73db6dac9af0fd554298097d358150ee385
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-11-30 00:53:08 +0800

    Sort variables

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const kScaffoldBackgroundColor = Color(0xff0a0d22);
 const kDefaultBoxColor = Color(0xff111428);
-const kGenderBoxColor = Color(0xff1d1f33);
-const kLabelColor = Color(0xffffffff);
-const kUnselectedLabelColor = Color(0xff8d8e98);
 const kDefaultButtonColor = Color(0xffeb1555);
-const kLargeFontSize = 36.0;
+const kGenderBoxColor = Color(0xff1d1f33);
 const kIconButtonColor = Color(0xff1c1f32);
+const kLabelColor = Color(0xffffffff);
+const kLargeFontSize = 36.0;
+const kScaffoldBackgroundColor = Color(0xff0a0d22);
+const kUnselectedLabelColor = Color(0xff8d8e98);
 
 enum Gender {
   male,
