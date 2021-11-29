5cb3af201dd4c5f434a135f2742c9e0f6e9cb812
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-11-30 00:49:35 +0800

    Added constant variables

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -6,3 +6,10 @@ const kGenderBoxColor = Color(0xff1d1f33);
 const kLabelColor = Color(0xffffffff);
 const kUnselectedLabelColor = Color(0xff8d8e98);
 const kDefaultButtonColor = Color(0xffeb1555);
+const kLargeFontSize = 36.0;
+const kIconButtonColor = Color(0xff1c1f32);
+
+enum Gender {
+  male,
+  female,
+}
